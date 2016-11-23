1718087986bf3024b99b6550b3230c5b0aa3822e
erick <erick.chemevski@gmail.com>
2016-11-23 09:21:50 -0200

    projeto

---
+++ Ordene/app/src/main/java/go/erick/ordene/MainActivity.java
@@ -0,0 +1,13 @@
+package go.erick.ordene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
