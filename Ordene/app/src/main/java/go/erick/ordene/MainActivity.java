2f19768d7dcc5bb38dd5c013da41c45f939f9edd
erick <erick.chemevski@gmail.com>
2016-11-25 12:22:56 -0200

    Atualizacoes

--- Ordene/app/src/main/java/go/erick/ordene/MainActivity.java
+++ Ordene/app/src/main/java/go/erick/ordene/MainActivity.java
@@ -2,6 +2,12 @@ package go.erick.ordene;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button iniciar = (Button) findViewById (R.id.iniciar);
+        Button sair = (Button) findViewById (R.id.sair);
+
+        iniciar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BubbleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sair.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
